062b031f4f50e53e7ad20d65a5277e83e3175866
Chan Gavin <justin.coding@gmail.com>
2020-06-30 18:34:26 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import "tailwindcss/dist/base.css";
+import "styles/globalStyles.css";
+import React from "react";
+import { css } from "styled-components/macro"; //eslint-disable-line
+
+/*
+ * This is the entry point component of this project. You can change the below exported default App component to any of
+ * the prebuilt landing page components by uncommenting their import and export lines respectively.
+ * See one of the landing page components to better understand how to import and render different components (Always
+ * make sure if you are building your own page, the root component should be the AnimationRevealPage component. You can
+ * disable the animation by using the disabled prop.
+ *
+ * The App component below is using React router to render the landing page that you see on the live demo website
+ * and the component previews.
+ *
+ */
+
+/* Use AnimationRevealPage as a wrapper component for your pages if you are custom building it */
+// import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+
+/*
+ * Hero section is the top most section on the page. It contains the header as well.
+ * So you dont need to import headers
+ * separately
+ */
+
+import SaaSProductLandingPage from "demos/SaaSProductLandingPage.js";
+import AboutPage from "pages/AboutUs";
+import PricingPage from "pages/Pricing";
+import BlogPage from "pages/BlogIndex";
+import AuthPage from "pages/Signup";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+export default function App() {
+  // return <AnimationRevealPage disabled></AnimationRevealPage>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <AboutPage />
+        </Route>
+        <Route path="/pricing">
+          <PricingPage />
+        </Route>
+        <Route path="/blog">
+          <BlogPage />
+        </Route>
+        <Route path="/auth">
+          <AuthPage />
+        </Route>
+        <Route path="/">
+          <SaaSProductLandingPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
---
+++ src/demos/SaaSProductLandingPage.js
@@ -0,0 +1,159 @@
+import React from "react";
+import tw from "twin.macro";
+import { css } from "styled-components/macro"; //eslint-disable-line
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+import Hero from "components/hero/TwoColumnWithInput.js";
+import Features from "components/features/ThreeColWithSideImage.js";
+import MainFeature from "components/features/TwoColWithButton.js";
+import MainFeature2 from "components/features/TwoColWithTwoHorizontalFeaturesAndButton.js";
+import FeatureWithSteps from "components/features/TwoColWithSteps.js";
+import Pricing from "components/pricing/ThreePlans.js";
+import Testimonial from "components/testimonials/TwoColumnWithImageAndRating.js";
+import FAQ from "components/faqs/SingleCol.js";
+import GetStarted from "components/cta/GetStarted";
+import Footer from "components/footers/FiveColumnWithBackground.js";
+import heroScreenshotImageSrc from "images/hero-screenshot-1.png";
+import macHeroScreenshotImageSrc from "images/hero-screenshot-2.png";
+import prototypeIllustrationImageSrc from "images/prototype-illustration.svg";
+import { ReactComponent as BriefcaseIcon } from "feather-icons/dist/icons/briefcase.svg";
+import { ReactComponent as MoneyIcon } from "feather-icons/dist/icons/dollar-sign.svg";
+
+export default () => {
+  const Subheading = tw.span`uppercase tracking-widest font-bold text-primary-500`;
+  const HighlightedText = tw.span`text-primary-500`;
+  const CopyrightText = tw.p`text-center`;
+
+  return (
+    <AnimationRevealPage>
+      <Hero roundedHeaderButton={true} />
+      <Features
+        subheading={<Subheading>Features</Subheading>}
+        heading={
+          <>
+            We have Amazing <HighlightedText>Features.</HighlightedText>
+          </>
+        }
+      />
+      <Pricing
+        subheading={<Subheading>Pricing</Subheading>}
+        heading={
+          <>
+            Reasonable & Flexible <HighlightedText>Plans.</HighlightedText>
+          </>
+        }
+        plans={[
+          {
+            name: "Free",
+            price: "$0",
+            duration: "Monthly",
+            mainFeature: "For Individuals",
+            features: [
+              "unblock Medium paywall",
+              "3 basic theme",
+              "classic comment system",
+              "export single page to pdf/word"
+            ]
+          },
+          {
+            name: "Pro",
+            price: "$3.99",
+            duration: "Monthly",
+            mainFeature: "For Small Businesses",
+            features: [
+              "ALL FREE FEATURE",
+              "more powerful classic comment system, including emoji support",
+              "technical support",
+              "batch export to pdf/word"
+            ],
+            featured: true
+          },
+          {
+            name: "Ultimate",
+            price: "$37.99",
+            duration: "Yearly",
+            mainFeature: "For Large Companies",
+            features: [
+              "ALL PRO FEATURE",
+              "export to ebook from Medium Publications"
+            ]
+          }
+        ]}
+      />
+      <Testimonial
+        subheading={<Subheading>Testimonials</Subheading>}
+        heading={
+          <>
+            Our Clients <HighlightedText>Love Us.</HighlightedText>
+          </>
+        }
+        testimonials={[
+          {
+            stars: 5,
+            profileImageSrc:
+              "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3.25&w=512&h=512&q=80",
+            heading: "Amazing User Experience",
+            quote:
+              "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+            customerName: "Charlotte Hale",
+            customerTitle: "Director, Delos Inc."
+          },
+          {
+            stars: 5,
+            profileImageSrc:
+              "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=512&h=512&q=80",
+            heading: "Love the Developer Experience and Design Principles !",
+            quote:
+              "Sinor Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+            customerName: "Adam Cuppy",
+            customerTitle: "Founder, EventsNYC"
+          }
+        ]}
+      />
+      <FAQ
+        subheading={<Subheading>FAQS</Subheading>}
+        heading={
+          <>
+            You have <HighlightedText>Questions ?</HighlightedText>
+          </>
+        }
+        faqs={[
+          {
+            question: "Are all the templates easily customizable ?",
+            answer:
+              "Yes, they all are. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          },
+          {
+            question:
+              "How long do you usually support an standalone template for ?",
+            answer:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          },
+          {
+            question: "What kind of payment methods do you accept ?",
+            answer:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          },
+          {
+            question:
+              "Is there a subscribption service to get the latest templates ?",
+            answer:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          },
+          {
+            question: "Are the templates compatible with the React ?",
+            answer:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          },
+          {
+            question: "Do you really support Internet Explorer 11 ?",
+            answer:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          }
+        ]}
+      />
+      <GetStarted />
+      {/* <Footer /> */}
+      <CopyrightText>&copy; Medium Enhanced Reader 2020</CopyrightText>
+    </AnimationRevealPage>
+  );
+};
